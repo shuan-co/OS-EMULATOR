6749369f2aa53db81fcafe9329531de4928949a2
Shuan Co <78131504+shuan-co@users.noreply.github.com>
2024-11-29 16:24:34 +0800

    Resolved Mutex Locks

--- src/process.h
+++ src/process.h
@@ -806,7 +806,8 @@ private:
                 if (processQueue.isEmpty())
                 {
                     idleCpuTicks++;
-                    Sleep(500);
+                    lock.unlock(); 
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
                     continue;
                 }
 
